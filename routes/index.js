89d85878ac55db9bbd31966b5ac6e98fa9124bca
youjie <youjie@hudongpai.com>
2019-01-17 21:10:23 +0800

    face++本地测试

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require("express");
+const axios = require("axios");
 var router = express.Router();
 
 /* GET home page. */
@@ -15,6 +16,10 @@ var APP_ID = "15433482";
 var API_KEY = "QarZMYI7ZCrH31GAnO5zKWZQ";
 var SECRET_KEY = "b5U98hpjpmfHxWVOQ1fI68IP8Wo07AVN";
 //这三个key记得替换为你申请的appid
+
+//face++ 的授权
+var FAPI_KEY = "T_ohwyZ4qPexYQGOM6Qpp3-8tRxums_U";
+var FSECRET_KEY = "hlXd8o2G_e_Q6_1FlXvBt09dohPQ9zg-";
 
 var client = new AipFace(APP_ID, API_KEY, SECRET_KEY);
 
@@ -58,4 +63,39 @@ router.post("/file/uploading", function(req, res, next) {
   });
 });
 
-module.exports = router;
+//face++本地测试
+var imageFace = fs.readFileSync("./public/images/face/1.jpg");
+var base64ImgFace = new Buffer(imageFace).toString("base64");
+
+var imageTpl = fs.readFileSync("./public/images/face/timg.jpg");
+var base64ImgTpl = new Buffer(imageTpl).toString("base64");
+
+const qs = require("qs");
+axios({
+  method: "post",
+  url: "https://api-cn.faceplusplus.com/imagepp/v1/mergeface",
+  data: qs.stringify({
+    api_key: FAPI_KEY,
+    api_secret: FSECRET_KEY,
+    template_base64: base64ImgTpl,
+    merge_base64: base64ImgFace
+  })
+})
+  .then(data => {
+    console.log(data);
+    var imgData = data.data.result;
+    //过滤data:URL
+    var base64Data = imgData.replace(/^data:image\/\w+;base64,/, "");
+    var dataBuffer = new Buffer(base64Data, "base64");
+    fs.writeFile("image.png", dataBuffer, function(err) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send("保存成功！");
+      }
+    });
+  })
+  .catch(e => {
+    console.log(e.response.data);
+  });
+https: module.exports = router;
